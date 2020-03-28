1e86ea99dd2ba63e4e14a5b2d9e5068f3c2fb79f
nguyentra908 <51397791+nguyentra908@users.noreply.github.com>
2020-03-28 23:06:45 +0700

    create solution structer

---
+++ WebShop.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WebShop.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ WebShop.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WebShop.Data
+{
+    public class Class1
+    {
+    }
+}
