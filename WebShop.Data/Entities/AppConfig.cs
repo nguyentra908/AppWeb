4adc8a2c6202be92ea27a8b55a5ed7944d0c2154
nguyentra908 <51397791+nguyentra908@users.noreply.github.com>
2020-04-08 08:31:56 +0700

    delete

--- WebShop.Data/Entities/AppConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebShop.Data.Configuration
-{
-    public class AppConfig
-    {
-        public int Key { get; internal set; }
-        public string Value { get; internal set; }
-    }
-}
